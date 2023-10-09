b83fe70ca818bf963aaa370985b33d46dde735bb
haoyu-ge <g2534160060@163.com>
2023-10-09 16:28:41 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'bilibili',
+  description: '这是nuxt的访bilibili',
+  keywords: 'bilibili, nuxt, vue',
+  referrer:'no-referrer'
+})
+</script>
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss">
+// 基础样式
+@import './assets/styles/base.scss';
+// 字体图标
+@import './assets/styles/iconfont.scss';
+:root{
+  --van-primary-color: #fb7299 !important;
+  --van-back-top-background: #fbfbfb !important;
+  --van-back-top-text-color: #666 !important;
+}
+</style>
---
+++ server/api/channel.get.ts
@@ -0,0 +1,5 @@
+// get /api/channel
+import channel from '@/database/channel'
+export default defineEventHandler(() => {
+  return channel
+})
